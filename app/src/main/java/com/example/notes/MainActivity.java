77c878d7d127b7f1567e277f83c4f91990862a18
disha <disha@orchids.edu.in>
2020-01-27 12:38:04 +0530

    initial commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.notes;
+
+import android.app.Application;
+
+public class MainActivity extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
